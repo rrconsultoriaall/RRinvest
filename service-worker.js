968bc15915ab086c581078139ab08b575abd61ee
rrconsultoriaall <rrconsultoria.all@gmail.com>
2026-03-11 20:29:06 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,48 @@
-const CACHE_NAME = "rrinvest-cache-v1"
+const CACHE_NAME = "rrinvest-cache-v2";
 
 const urlsToCache = [
   "/",
   "/index.html",
+  "/login.html",
   "/style.css",
   "/config.js",
-  "/login.html"
-]
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  )
-})
+    caches.open(CACHE_NAME).then(async cache => {
+      for (const url of urlsToCache) {
+        try {
+          await cache.add(url);
+        } catch (e) {
+          console.log("Falha ao cachear:", url);
+        }
+      }
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  )
-})
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
